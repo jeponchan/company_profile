a655ab9559bcef9d7564cda48afe8f454d60ebb0
jeponchan <147708244+jeponchan@users.noreply.github.com>
2025-07-27 20:10:46 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = ""; // jika kamu tidak pakai password di phpMyAdmin
-$dbname = "karyalestari_db"; // atau sesuaikan dengan nama databasenya
+$host = getenv('DB_HOST');        // e.g. sql7.freesqldatabase.com
+$user = getenv('DB_USER');        // e.g. sql7792186
+$pass = getenv('DB_PASSWORD');    // password kamu
+$db   = getenv('DB_NAME');        // e.g. sql7792186
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
 
+// Cek koneksi
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
 }
